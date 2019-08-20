78820c971287df1b32bac326792b5e756a95d1d0
shallun <54306102+shallun@users.noreply.github.com>
2019-08-20 13:21:05 +0530

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,10 @@
+def boole(num1,num2,num3):
+        if(num1>num2 and num1>num3):
+                print(num1,"is greatest")
+        elif(num2>num3 and num2>num1):
+                print(num2,"is greatest")
+        else:
+                print(num3,"is greatest")
+
+
+                                
